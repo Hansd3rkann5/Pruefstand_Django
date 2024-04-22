030eb01c5b3050d819ac568761c3f5a21920293a
simonbader <simon-bader@gmx.net>
2024-04-22 13:00:01 +0200

    strip

--- pruefstand/consumers copy.py
+++ pruefstand/consumers copy.py
@@ -1,4 +1,6 @@
 import yaml
+import itertools
+import time
 
 cons = ["Motor", "Display", "Battery", "Charger", "Range EXT", "Service Dongle"]
 choice = ['Wahl 1', 'Wahl']
@@ -22,7 +24,7 @@ with open(f"komp_pruefstand/static/Komponenten/Komponenten.yaml", "r") as y:
                 comp[0] = 'Range EXT'
             if komma != -1:
                 variants = [''] * len(konfig[comp[0]])
-                choices = [x.strip() for x in comp[1].split(',', comp[1].find(','))]
+                choices = [x.strip() for x in comp[1].split(',')]
                 comp = comp[0]
                 for i in range(len(konfig[comp])):
                     variants[i] = konfig[comp][i]['name']   #Array mit Namen der Unterkomponenten, von Komp, wo Aushwahl > 1
@@ -31,12 +33,14 @@ with open(f"komp_pruefstand/static/Komponenten/Komponenten.yaml", "r") as y:
                 for name in range(len(choices)):
                     if choices[name].strip() not in variants:
                         odds.append((choices[name].strip()))
-                if len(odds) > 1:
+                if len(odds) <= 1:
                     num = 'sind'
+                    num1 = 'ist'
                 else:
                     num = 'ist'
+                    num1 = 'sind'
                 print(f'Davon {num} aber nur {len(choices) - len(odds)} gültig.')
-                print(f'{comp} {odds} {num} ungültig')
+                print(f'{comp} {odds} {num1} ungültig')
                 for i in range(len(choices)):
                     for index in range(len(konfig[comp])):
                         k = konfig[comp][index]['name']
@@ -50,7 +54,19 @@ with open(f"komp_pruefstand/static/Komponenten/Komponenten.yaml", "r") as y:
                 print(f'{comp[0]}, {comp[1]} hat Relay: {konfig[comp[0]][0]["relay"]}')
         print(f'Relays zu schalten: {relays}')
 
-for key in relays:
-    if relays[key] != None:
-        if len(relays[key]) != 1:
-            print(f'{key} hat mehr als 1')
+
+# l = []
+# for key in relays:
+#     if relays[key] != None:
+#         l.append(relays[key])
+#     else:
+#         l.append([-1])
+# print(l)
+# comb = list(itertools.product(*l))
+# print(comb)
+# for combination in comb:
+#     print(combination)
+#     for rel in combination:
+#         print(rel)
+#         #print(comb[combination][rel])
+#     time.sleep(1)
--- pruefstand/consumers.py
+++ pruefstand/consumers.py
@@ -5,6 +5,7 @@ import serial
 import pruefstand.pycrc as pycrc
 import time
 import yaml
+import itertools
 from django.http import JsonResponse
 from threading import Thread
 from channels.generic.websocket import WebsocketConsumer
@@ -93,12 +94,15 @@ class TestConsumer(WebsocketConsumer):
         self.running = False
         self.modbus = ModBusRelay()
         self.progress = 0
+        self.test = ''
+        self.combinations = []
         
     def connect(self):
         self.accept()
         self.running = True
         self.modbus.reset_all()
         self.send_num()
+        self.test = ''
     
     #Funktion zum Senden der Nachrichten an Client - verallgemeinert mit "Message" und "Value"
     def _send(self, message, value):
@@ -123,9 +127,11 @@ class TestConsumer(WebsocketConsumer):
                 type = text_data_json["type"]
             if type == "auto":
                 print("Automatisierter Durchlauf")
+                self.test = type
                 type = text_data_json["type"]
             if type == "manuell":
                 print("Manueller Durchlauf")
+                self.test = type
                 type = text_data_json["type"]
             if type in self.modbus.cons:
                 id = text_data_json["id"]
@@ -137,13 +143,13 @@ class TestConsumer(WebsocketConsumer):
             if type == "Konfig-File":
                 print("Konfig-File recieved")
                 konfig = text_data_json["text"]
-                try:
-                    f = open("komp_pruefstand/static/Komponenten/Konfig.txt", "w")
-                    f.write(konfig)
-                    f.close()
-                    self.check_konfig_file()
-                except Exception as e:
-                    show_error(e)
+                #try:
+                f = open("komp_pruefstand/static/Komponenten/Konfig.txt", "w")
+                f.write(konfig)
+                f.close()
+                self.check_konfig_file()
+                #except Exception as e:
+                #    show_error(e)
                 #print(konfig)
             if type == "stop":
                 print("stop")
@@ -175,51 +181,73 @@ class TestConsumer(WebsocketConsumer):
             show_error(e)
 
     def check_konfig_file(self):
-        self._send('Upload erfolgreich', 0)
-        with open(f"komp_pruefstand/static/Komponenten/Komponenten.yaml", "r") as y:
-            with open(f"komp_pruefstand/static/Komponenten/Konfig.txt", "r") as t:
-                konfig = yaml.safe_load(y)
-                cons = list(konfig.keys())
-                p = t.readlines()
-                relays = {}
-                for key in cons:
-                    relays[key] = None
-                for line in p:
-                    string = line.rstrip()
-                    relay = []
-                    comp = [x.strip() for x in string.split(':', line.find(':'))]
-                    komma = line.find(",")
-                    if comp[0] == 'Range Ext' or comp[0] == 'RangeExt':
-                        comp[0] = 'Range EXT'
-                    if komma != -1:
-                        variants = [''] * len(konfig[comp[0]])
-                        choices = [x.strip() for x in comp[1].split(',', comp[1].find(','))]
-                        comp = comp[0]
-                        for i in range(len(konfig[comp])):
-                            variants[i] = konfig[comp][i]['name']   #Array mit Namen der Unterkomponenten, von Komp, wo Aushwahl > 1
-                        print(f'\nAnzahl Wahl {comp}: {len(choices)}')
-                        odds = []
-                        for name in range(len(choices)):
-                            if choices[name].strip() not in variants:
-                                odds.append((choices[name].strip()))
-                        if len(odds) > 1:
-                            num = 'sind'
-                        else:
-                            num = 'ist'
-                        print(f'Davon {num} aber nur {len(choices) - len(odds)} gültig.')
-                        print(f'{comp} {odds} {num} ungültig')
-                        for i in range(len(choices)):
-                            for index in range(len(konfig[comp])):
-                                k = konfig[comp][index]['name']
-                                if k == choices[i].strip():
-                                    relay.append((konfig[comp][index]["relay"]))
-                                    print(f'{comp}, {choices[i]} hat Relay: {konfig[comp][index]["relay"]}')
-                        relay.sort()
-                        relays[comp] = relay
-                    elif comp[1] != 'None':
-                        relays[comp[0]] = [(konfig[comp[0]][0]["relay"])]
-                        print(f'{comp[0]}, {comp[1]} hat Relay: {konfig[comp[0]][0]["relay"]}')
-                print(f'Relays zu schalten: {relays}')
+        try:
+            self._send('Upload erfolgreich', 0)
+            with open(f"komp_pruefstand/static/Komponenten/Komponenten.yaml", "r") as y:
+                with open(f"komp_pruefstand/static/Komponenten/Konfig.txt", "r") as t:
+                    konfig = yaml.safe_load(y)
+                    cons = list(konfig.keys())
+                    p = t.readlines()
+                    relays = {}
+                    for key in cons:
+                        relays[key] = None
+                    for line in p:
+                        string = line.rstrip()
+                        relay = []
+                        comp = [x.strip() for x in string.split(':', line.find(':'))]
+                        komma = line.find(",")
+                        if comp[0] == 'Range Ext' or comp[0] == 'RangeExt':
+                            comp[0] = 'Range EXT'
+                        if komma != -1:
+                            variants = [''] * len(konfig[comp[0]])
+                            choices = [x.strip() for x in comp[1].split(','0)]
+                            comp = comp[0]
+                            for i in range(len(konfig[comp])):
+                                variants[i] = konfig[comp][i]['name']   #Array mit Namen der Unterkomponenten, von Komp, wo Aushwahl > 1
+                            print(f'\nAnzahl Wahl {comp}: {len(choices)}')
+                            odds = []
+                            for name in range(len(choices)):
+                                if choices[name].strip() not in variants:
+                                    odds.append((choices[name].strip()))
+                            if len(odds) > 1:
+                                num = 'sind'
+                            else:
+                                num = 'ist'
+                            if len(odds) != 0:
+                                print(f'Davon {num} aber nur {len(choices) - len(odds)} gültig.')
+                                print(f'{comp} {odds} {num} ungültig')
+                            for i in range(len(choices)):
+                                for index in range(len(konfig[comp])):
+                                    k = konfig[comp][index]['name']
+                                    if k == choices[i].strip():
+                                        relay.append((konfig[comp][index]["relay"]))
+                                        print(f'{comp}, {choices[i]} hat Relay: {konfig[comp][index]["relay"]}')
+                            relay.sort()
+                            relays[comp] = relay
+                        elif comp[1] != 'None':
+                            relays[comp[0]] = [(konfig[comp[0]][0]["relay"])]
+                            print(f'{comp[0]}, {comp[1]} hat Relay: {konfig[comp[0]][0]["relay"]}')
+                    print(f'Relays zu schalten: {relays}')
+            l = []
+            if self.test == "auto":
+                for key in relays:
+                    if relays[key] != None:
+                        l.append(relays[key])
+                    else:
+                        l.append([-1])
+                self.combinations = list(itertools.product(*l))
+                i = 1
+                print(f'\nAnzahl der Kombinationen: {len(self.combinations)}')
+                for combination in self.combinations:
+                    print(f'Combination: {i}')
+                    i += 1
+                    for rel in range(len(combination)):
+                        self.modbus.serial_write(rel)
+                        time.sleep(0.2)
+                    time.sleep(1)
+                print('Alle Kombinatoriken geschalten')
+        except Exception as e:
+            show_error(e)
                 
     def stop(self):
         self.running = False
